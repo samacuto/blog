c4cbf2d59cb03921af22321b37b5b7c7f62a4a47
Samuel Cuevas Torres <146704034+samuelcut01@users.noreply.github.com>
2025-11-23 15:52:05 +0100

    Update site configuration with correct title, author, and locale

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -2,35 +2,35 @@ import type {
   SiteConfiguration,
   NavigationLinks,
   SocialLinks,
-} from '@/types.ts'
+} from "@/types.ts";
 
 export const SITE: SiteConfiguration = {
-  title: 'Barebones',
+  title: "Blog de samacuto",
   description:
-    'A barebones starter theme. Built with Astro, Tailwind CSS, and Markdown.',
-  href: 'https://barebones.superwebthemes.com',
-  author: 'SuperWeb Development Inc.',
-  locale: 'en-CA',
-}
+    "A barebones starter theme. Built with Astro, Tailwind CSS, and Markdown.",
+  href: "https://barebones.superwebthemes.com",
+  author: "samacuto",
+  locale: "es-ES",
+};
 
 export const NAV_LINKS: NavigationLinks = {
   blog: {
-    path: '/blog',
-    label: 'Blog',
+    path: "/blog",
+    label: "Blog",
   },
-}
+};
 
 export const SOCIAL_LINKS: SocialLinks = {
   email: {
-    label: 'Email',
-    href: 'mailto:ttl@trevortylerlee.com',
+    label: "Email",
+    href: "mailto:ttl@trevortylerlee.com",
   },
   github: {
-    label: 'GitHub',
-    href: 'https://github.com/trevortylerlee',
+    label: "GitHub",
+    href: "https://github.com/trevortylerlee",
   },
   twitter: {
-    label: 'X (formerly Twitter)',
-    href: 'https://twitter.com/boogerbuttcheek',
+    label: "X (formerly Twitter)",
+    href: "https://twitter.com/boogerbuttcheek",
   },
-}
+};
